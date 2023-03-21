33b8bee277d3b4259a87333e52ee5ec0ad571d33
techygrrrl <techygrrrl@proton.me>
2023-03-20 22:21:49 -0700

    save config to user config dir instead of locally

--- utils/file.go
+++ utils/file.go
@@ -2,13 +2,10 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 
 	"github.com/techygrrrl/timerrr/main/models"
-)
-
-const (
-	timersJsonFileName = "timers.json"
 )
 
 func AddTimer(timer models.SavedTimer) error {
@@ -27,10 +24,16 @@ func RemoveTimerAtIndex(index int) error {
 	return persistTimers(timers)
 }
 
+// LoadTimersFromFile
+// Load timers from file. If any error occurs, ignore it and return an empty data structure
 func LoadTimersFromFile() []models.SavedTimer {
-	data, err := os.ReadFile(timersJsonFileName)
+	configPath, err := ConfigFilePath()
 	if err != nil {
-		// Empty file, return empty array
+		return []models.SavedTimer{}
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
 		return []models.SavedTimer{}
 	}
 
@@ -44,10 +47,36 @@ func LoadTimersFromFile() []models.SavedTimer {
 }
 
 func persistTimers(timers []models.SavedTimer) error {
+	configPath, err := ConfigFilePath()
+	if err != nil {
+		return err
+	}
+
 	data, err := json.MarshalIndent(timers, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	return os.WriteFile(timersJsonFileName, data, 0644)
+	return os.WriteFile(configPath, data, 0644)
+}
+
+// ConfigFilePath
+// Returns the desired configuration path for the file.
+// on Mac, it's the Application Support, in Linux it's
+func ConfigFilePath() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	configDir = configDir + "/timerrr"
+
+	if _, err := os.Stat(configDir); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(configDir, os.ModePerm)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return configDir + "/timers.json", nil
 }
