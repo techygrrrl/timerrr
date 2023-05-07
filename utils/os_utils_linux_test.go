59cbe8e55c45e6604c2aca183a7253e35cb160b9
Allison Durham <AdjetiveAllison@gmail.com>
2023-05-07 12:26:17 -0500

    Added example test for supportin runner/local

--- utils/os_utils_linux_test.go
+++ utils/os_utils_linux_test.go
@@ -15,18 +15,26 @@ func TestIsLinux(t *testing.T) {
 
 func TestUserConfigDirLinux(t *testing.T) {
 	path, err := os.UserConfigDir()
+	home := os.Getenv("HOME")
+	if home == "" {
+		home = "/home/runner"
+	}
 
 	t.Logf("🧵 Config dir: %s", path)
 
 	assert.Nil(t, err)
-	assert.Equal(t, "/home/runner/.config", path)
+	assert.Equal(t, fmt.Sprintf("%s/.config", home), path)
 }
 
 func TestUserConfigFilePathLinux(t *testing.T) {
 	path, err := ConfigFilePath()
+	home := os.Getenv("HOME")
+	if home == "" {
+		home = "/home/runner"
+	}
 
 	t.Logf("🧵 Config file path: %s", path)
 
 	assert.Nil(t, err)
-	assert.Equal(t, "/home/runner/.config/timerrr/timers.json", path)
+	assert.Equal(t, fmt.Sprintf("%s/.config/timerrr/timers.json", home), path)
 }
