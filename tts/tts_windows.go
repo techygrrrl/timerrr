a4b42e710ceca6e7cdd87c74a19d3601fefaccc9
@techygrrrl <techygrrrl@proton.me>
2023-10-03 18:31:49 -0700

    add TTS support for windows

--- tts/tts_windows.go
+++ tts/tts_windows.go
@@ -1,9 +1,11 @@
 package tts
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func TtsCommand(message string) *exec.Cmd {
-	return exec.Command("echo", message)
+	speakCmd := fmt.Sprintf("(New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('%s');", message)
+	return exec.Command("PowerShell", "-Command", "Add-Type", "-AssemblyName", "System.Speech;", speakCmd)
 }
